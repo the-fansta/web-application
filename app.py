843a7e381aee61ebc00dcab8b8431b55a1faf1fb
the-fansta <80322453+the-fansta@users.noreply.github.com>
2021-09-21 16:43:36 +0530

    0.1
    0.1

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app.config["SECRET_KEY"]="1234"
 @app.route('/')
 @app.route('/home')
 def hello():
-    return render_template("index.html")
+    return render_template("home.html")
 
-@app.route('/aboutme')
-def aboutme():
-    return render_template('aboutme.html')
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 @app.route('/register')
 def register():
     return render_template('registration.html')
 
-@app.route('/validate_register',methods=['POST'])
+@app.route('/validate_register',methods=['POST',"GET"])
 def validate_register():
     username=request.form.get('username')
     email=request.form.get('password')
@@ -40,7 +40,7 @@ def validate_register():
 def login_page():
    return render_template("login_page.html")
 
-@app.route('/validate_user',methods=['POST'])
+@app.route('/validate_user',methods=['POST',"GET"])
 def validate_user():
     user_name=request.form.get("username")
     password=request.form.get("password")
@@ -103,7 +103,7 @@ def validatequiz(correct_answer):
         message="Excellent, You are Right!!"
         score=score+20
     else:
-        message="Nice try,The right answer is B"
+        message="Nice try,The right answer is "+ correct_answer
     flash(message)
     session["user_score"]=score
     return message
@@ -114,88 +114,118 @@ def validatequiz(correct_answer):
 
 #--------------------Q1-------------------------------------------
 
-@app.route('/get_q1')
-def get_q1():
-    score=0
-    return render_template("question-1.html",score=score)
     
-@app.route('/q1',methods=['POST'])
+@app.route('/q1',methods=['POST',"GET"])
 def q1():
-   message = validatequiz('2')
+   if request.method=="GET":
+        return render_template("question-1.html")
+   message = validatequiz('3')
    score=session.get("user_score",0)
    return render_template("question-2.html",message=message,score=score)
 
 #--------------------Q2-------------------------------------------
 
-@app.route('/q2',methods=['POST'])
+@app.route('/q2',methods=['POST',"GET"])
 def q2():
-    message = validatequiz('2')
+    if request.method=="GET":
+        return render_template("question-2.html")
+    message = validatequiz('3')
     score=session.get("user_score",0)
     return render_template("question-3.html",message=message,score=score)
 
 #--------------------Q3-------------------------------------------
 
-@app.route('/q3',methods=['POST'])
+@app.route('/q3',methods=['POST',"GET"])
 def q3():
+    if request.method=="GET":
+        return render_template("question-3.html")
     message = validatequiz('3')
     score=session.get("user_score",0)
     return render_template("question-4.html",message=message,score=score)
 
 #--------------------Q4-------------------------------------------
 
-@app.route('/q4',methods=['POST'])
+@app.route('/q4',methods=['POST',"GET"])
 def q4():
-    message = validatequiz('4')
+    if request.method=="GET":
+        return render_template("question-4.html")
+    message = validatequiz('2')
     score=session.get("user_score",0)
     return render_template("question-5.html",message=message,score=score)
 
 #--------------------Q5-------------------------------------------
 
-@app.route('/q5',methods=['POST'])
+@app.route('/q5',methods=['POST',"GET"])
 def q5():
-    message = validatequiz('2')
+    if request.method=="GET":
+        return render_template("question-5.html")
+    message = validatequiz('3')
     score=session.get("user_score",0)
     return render_template("question-6.html",message=message,score=score)
 
 #--------------------Q6-------------------------------------------
 
-@app.route('/q6',methods=['POST'])
+@app.route('/q6',methods=['POST',"GET"])
 def q6():
-    message = validatequiz('4')
+    if request.method=="GET":
+        return render_template("question-6.html")
+    message = validatequiz('2')
     score=session.get("user_score",0)
     return render_template("question-7.html",message=message,score=score)
 
 #--------------------Q7-------------------------------------------
 
-@app.route('/q7',methods=['POST'])
+@app.route('/q7',methods=['POST',"GET"])
 def q7():
-    message = validatequiz('4')
+    if request.method=="GET":
+        return render_template("question-7.html")
+    message = validatequiz('2')
     score=session.get("user_score",0)
     return render_template("question-8.html",message=message,score=score)
 
 #--------------------Q8-------------------------------------------
 
-@app.route('/q8',methods=['POST'])
+@app.route('/q8',methods=['POST',"GET"])
 def q8():
-    message = validatequiz('4')
+    if request.method=="GET":
+        return render_template("question-8.html")
+    message = validatequiz('3')
     score=session.get("user_score",0)
     return render_template("question-9.html",message=message,score=score)
 
 #--------------------Q9-------------------------------------------
 
-@app.route('/q9',methods=['POST'])
+@app.route('/q9',methods=['POST',"GET"])
 def q9():
-    message = validatequiz('2')
+    if request.method=="GET":
+        return render_template("question-9.html")
+    message = validatequiz('1')
     score=session.get("user_score",0)
     return render_template("question-10.html",message=message,score=score)
 
 #--------------------Q10-------------------------------------------
 
-@app.route('/q10',methods=['POST'])
+@app.route('/q10',methods=['POST',"GET"])
 def q10():
-    message = validatequiz('1')
+    if request.method=="GET":
+        return render_template("question-10.html")  
+    message = validatequiz('3')
     score=session.get("user_score",0)
-    return render_template("thank_you.html",message=message,score=score)
+    message="Your score is "+ str(score) 
+    return render_template("thank_you.html",new_message=message,score=score)
+
+#--------------------THANKU-------------------------------------------
+
+@app.route('/thanku')
+def thanku():
+    return render_template("thank_you.html")
+
+
+
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
